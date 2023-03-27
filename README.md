7ca53a8313eff4babd9b7680f93ee3ce433bfda5
Andrei <soprachev@mail.ru>
2023-03-27 18:57:25 +0300

    upd get

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # Yandex Cloud Worker Folder
+
+> Если вам нужно на каждый PR создавать тестовый инстанс вашей инфраструктуры в Yandex Cloud, то этот Action для вас.
+
+## Проблема
+
+Новые директории в облаке создаются и удаляются крайне **медленно**. По этому создавать на каждый PR директорию, а потом удалять её не выгодно по времени выполнения CI/CD.
+
+## Решение
+
+Вы можете иметь несколько рабочих директорий. На каждый PR резервировать свободную, а после завершения PR освобождать её.
+
+## Как это работает
+
+Этот Action имеет два режима работы:
+
+- allocate - резервирует рабочую директорию
+- free - освобождает рабочую директорию
+
+Состояние директории хранится в `labels` директории в Yandex Cloud. Если при резервирование директории не найдено свободной, то создаётся новая.
+
+Имя рабочих директорий формируется по шаблону: `worker-folder-ID`. Состояние отображается в описание директории `[working]` или `[free]`. Когда вы резервируете директорию, вы можете указать имя директории, которое будет использоваться вместо `ID`, например `worker-folder-PR-123`.
+
+## Пример использования
+
+### Резервирование директории
+
+```yaml
+name: CI
+
+on:
+  pull_request:
+    types: [opened]
+
+jobs:
+  create-pr-stand:
+    runs-on: ubuntu-latest
+    steps:
+      - name: allocate folder
+        id: folder
+        uses: soprachevak/yandex-cloud-worker-folder@v1
+        with:
+          operation: allocate
+          cloudId: ${{ secrets.YC_CLOUD_ID }}
+          oauthToken: ${{ secrets.YC_TOKEN }}
+          folderName: PR-${{ github.event.number }}
+
+      - name: deploy
+        run: echo "FOLDER_ID=${{ steps.folder.outputs.folderId }}"
+
+        ...
+```
+
+### Освобождение директории
+
+```yaml
+name: CI
+
+on:
+  pull_request:
+    types: [closed]
+
+jobs:
+  delete-pr-stand:
+    runs-on: ubuntu-latest
+    steps:
+      - name: free folder
+        id: folder
+        uses: soprachevak/yandex-cloud-worker-folder@v1
+        with:
+          operation: free
+          cloudId: ${{ secrets.YC_CLOUD_ID }}
+          oauthToken: ${{ secrets.YC_TOKEN }}
+          folderName: PR-${{ github.event.number }}
+```
--- index.js
+++ index.js
@@ -1,42 +1,59 @@
-import core from '@actions/core';
-import { Session, cloudApi, serviceClients } from '@yandex-cloud/nodejs-sdk';
+import core from '@actions/core'
+import github from '@actions/github'
+import { Session, cloudApi, serviceClients } from '@yandex-cloud/nodejs-sdk'
+import md5 from 'md5'
 
-const WORKER_NODE_TAG = 'worker-node';
-const WORKER_NODE_ID_TAG = 'worker-node-id';
+const WORKER_NODE_TAG = 'worker-node'
+const WORKER_NODE_ID_TAG = 'worker-node-id'
+const WORKER_KEY_TAG = 'worker-node-key'
 
 const {
   resourcemanager: {
     folder_service: { ListFoldersRequest, CreateFolderRequest, UpdateFolderRequest, GetFolderRequest, CreateFolderMetadata },
     folder: { Folder_Status },
   },
-} = cloudApi;
+} = cloudApi
 
 
 try {
-  const operation = core.getInput('operation', { required: true });
-  const cloudId = core.getInput('cloudId', { required: true });
-  const oauthToken = core.getInput('oauthToken', { required: true });
-  const allowNameChange = core.getInput('allowChangeName') ?? true;
-  const freeFolderId = core.getInput('folderId') ?? '';
-  const targetName = core.getInput('name') ?? '';
+  const operation = core.getInput('operation', { required: true })
+  const cloudId = core.getInput('cloudId', { required: true })
+  const oauthToken = core.getInput('oauthToken', { required: true })
+  const allowNameChange = core.getInput('allowChangeName') ?? true
+  const freeFolderId = core.getInput('folderId') ?? ''
+  const targetName = core.getInput('name') ?? ''
 
-  const folderService = new Session({ oauthToken }).client(serviceClients.FolderServiceClient);
+  const folderService = new Session({ oauthToken }).client(serviceClients.FolderServiceClient)
   const folders = await folderService.list(ListFoldersRequest.fromPartial({ cloudId }))
   const folderNames = folders.folders.map(folder => folder.name)
 
-  async function allocateWorker() {
+  async function getWorker(allowAllocate, key) {
     const workers = folders.folders.filter(folder =>
       folder.labels[WORKER_NODE_TAG] &&
       folder.labels[WORKER_NODE_ID_TAG] &&
-      folder.status == Folder_Status.ACTIVE);
+      folder.status == Folder_Status.ACTIVE)
 
-    const freeWorkers = workers.filter(folder => folder.labels[WORKER_NODE_TAG] == 'free');
-    const workerIds = workers.map(folder => folder.labels[WORKER_NODE_ID_TAG]);
+    console.log(`Use key: ${key}`)
+    const hashKey = md5(tag)
+
+    const workerByKey = workers.find(folder => folder.labels[WORKER_KEY_TAG] == hashKey)
+    if (workerByKey) {
+      console.log(`Found worker by key: ${workerByKey.id}`)
+      return workerByKey.id
+    }
+
+    if (allowAllocate == false) {
+      throw new Error(`No free workers found. Allocate new worker is not allowed`)
+    }
+
+    const freeWorkers = workers.filter(folder => folder.labels[WORKER_NODE_TAG] == 'free')
+    const workerIds = workers.map(folder => folder.labels[WORKER_NODE_ID_TAG])
 
     const workName = `worker-folder-${targetName}`
 
     if (freeWorkers.length == 0) {
-      const id = makeidUnique(4, workerIds);
+      console.log(`Found ${freeWorkers.length} free workers. Total workers: ${workers.length}. Allocate new worker...`);
+      const id = makeidUnique(4, workerIds)
 
       const folder = await folderService.create(CreateFolderRequest.fromPartial({
         cloudId: cloudId,
@@ -44,32 +61,35 @@ try {
         description: `[working] CICD worker folder ${id}`,
         labels: {
           [WORKER_NODE_TAG]: 'working',
-          [WORKER_NODE_ID_TAG]: id
+          [WORKER_NODE_ID_TAG]: id,
+          [WORKER_KEY_TAG]: hashKey
         }
-      }));
+      }))
 
-      return CreateFolderMetadata.decode(folder.metadata.value).folderId;
+      return CreateFolderMetadata.decode(folder.metadata.value).folderId
     }
 
-    const worker = freeWorkers[0];
+    console.log(`Found ${freeWorkers.length} free workers. Total workers: ${workers.length}. Use first free worker...`);
+    const worker = freeWorkers[0]
     await folderService.update(UpdateFolderRequest.fromPartial({
       folderId: worker.id,
       name: allowNameChange && !folderNames.includes(workName) ? workName : worker.name,
       description: allowNameChange ? `[working] CICD worker folder ${worker.labels[WORKER_NODE_ID_TAG]}` : worker.description,
       labels: {
         [WORKER_NODE_TAG]: 'working',
-        [WORKER_NODE_ID_TAG]: worker.labels[WORKER_NODE_ID_TAG]
+        [WORKER_NODE_ID_TAG]: worker.labels[WORKER_NODE_ID_TAG],
+        [WORKER_KEY_TAG]: hashKey
       }
     }))
 
-    return worker.id;
+    return worker.id
   }
 
   async function freeWorker(folderId) {
-    const folderService = new Session({ oauthToken }).client(serviceClients.FolderServiceClient);
-    const folder = await folderService.get(GetFolderRequest.fromPartial({ folderId }));
+    const folderService = new Session({ oauthToken }).client(serviceClients.FolderServiceClient)
+    const folder = await folderService.get(GetFolderRequest.fromPartial({ folderId }))
 
-    const id = folder.labels[WORKER_NODE_ID_TAG];
+    const id = folder.labels[WORKER_NODE_ID_TAG]
     const name = `worker-folder-${id}`
     await folderService.update(UpdateFolderRequest.fromPartial({
       folderId,
@@ -80,42 +100,76 @@ try {
         [WORKER_NODE_ID_TAG]: folder.labels[WORKER_NODE_ID_TAG]
       }
     }))
+
+    return folderId
+  }
+
+  async function freeWorkerByKey(key) {
+    const workers = folders.folders.filter(folder =>
+      folder.labels[WORKER_NODE_TAG] &&
+      folder.labels[WORKER_NODE_ID_TAG] &&
+      folder.status == Folder_Status.ACTIVE)
+
+    console.log(`Use key: ${key}`)
+    const hashKey = md5(tag)
+
+    const workerByKey = workers.find(folder => folder.labels[WORKER_KEY_TAG] == hashKey)
+    if (workerByKey) {
+      console.log(`Found worker by key: ${workerByKey.id}`)
+      freeWorker(workerByKey.id)
+      return workerByKey.id
+    }
+
+    throw new Error(`No worker found by key: ${key}`)
   }
 
   function makeid(length) {
-    let result = '';
-    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    const charactersLength = characters.length;
-    let counter = 0;
+    let result = ''
+    const characters = 'abcdefghijklmnopqrstuvwxyz0123456789'
+    const charactersLength = characters.length
+    let counter = 0
     while (counter < length) {
-      result += characters.charAt(Math.floor(Math.random() * charactersLength));
-      counter += 1;
+      result += characters.charAt(Math.floor(Math.random() * charactersLength))
+      counter += 1
     }
-    return result;
+    return result
   }
 
   function makeidUnique(length, ids) {
-    let id = makeid(length);
+    let id = makeid(length)
     while (ids.includes(id)) {
-      id = makeid(length);
+      id = makeid(length)
     }
-    return id;
+    return id
   }
 
-  if (operation != 'allocate' && operation != 'free') {
-    throw new Error(`Unknown operation: ${operation}. Expected 'allocate' or 'free'`);
+  function getKey() {
+    let key = core.getInput('key')
+
+    if (!key) {
+      const pr = github.context.payload.pull_request
+      if (!pr) throw new Error(`Key is required for operation 'get' when not running in a pull request context`)
+      key = `${github.context.repo.owner}/${github.context.repo.repo}-${github.context.payload.pull_request.number}`
+    }
+
+    return key
   }
 
-  if (operation == 'free' && freeFolderId == '') {
-    throw new Error(`Folder id is required for operation 'free'`);
+
+  const key = getKey()
+
+  if (operation != 'get' && operation != 'free') {
+    throw new Error(`Unknown operation: ${operation}. Expected 'allocate' or 'free'`)
   }
 
-  if (operation == 'allocate') {
-    const folderId = await allocateWorker();
-    core.setOutput("folderId", folderId);
-  } else if (operation == 'free') {
-    await freeWorker(freeFolderId);
-    core.setOutput("folderId", freeFolderId);
+  if (operation == 'get') {
+    const allowAllocate = core.getInput('allowAllocate') ?? true
+    const folderId = await getWorker(allowAllocate, key)
+    core.setOutput("folderId", folderId)
+  } else if (operation == 'free' && freeFolderId == '') {
+    core.setOutput("folderId", await freeWorkerByKey(key))
+  } else if (operation == 'free' && freeFolderId != '') {
+    core.setOutput("folderId", await freeWorker(freeFolderId))
   }
 
 } catch (error) {
